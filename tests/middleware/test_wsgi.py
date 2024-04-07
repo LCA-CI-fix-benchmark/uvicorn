1dc9171ee998047c787d18f55d352275320b463a
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 20:59:49 +0000

    test_lca_code_editing__code-editing-metrics-x688f9e9d9d296ff9__id_147

--- tests/middleware/test_wsgi.py
+++ tests/middleware/test_wsgi.py
@@ -49,10 +49,9 @@ def return_exc_info(environ: Environ, start_response: StartResponse) -> List[byt
         start_response(status, headers, sys.exc_info())  # type: ignore[arg-type]
         return [output]
 
-
 @pytest.fixture(params=[wsgi._WSGIMiddleware, a2wsgi.WSGIMiddleware])
 def wsgi_middleware(request: pytest.FixtureRequest) -> Callable:
-    return request.param
+    return request.param  # type: ignore
 
 
 @pytest.mark.anyio

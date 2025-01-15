c7fccb41a75c36a5d5d232cb33d3550e2c0a4d50
ubuntu <ubuntu@ip-10-0-1-33>
2025-01-15 16:42:04 +0000

    waleko__claude-3.5-sonnet_d261__id_147

--- tests/middleware/test_wsgi.py
+++ tests/middleware/test_wsgi.py
@@ -1,6 +1,6 @@
 import io
 import sys
-from typing import AsyncGenerator, Callable, List
+from typing import AsyncGenerator, Callable, List, Type, Union
 
 import a2wsgi
 import httpx
@@ -51,8 +51,8 @@ def return_exc_info(environ: Environ, start_response: StartResponse) -> List[byt
 
 
 @pytest.fixture(params=[wsgi._WSGIMiddleware, a2wsgi.WSGIMiddleware])
-def wsgi_middleware(request: pytest.FixtureRequest) -> Callable:
-    return request.param
+def wsgi_middleware(request: pytest.FixtureRequest) -> Type[Union[wsgi._WSGIMiddleware, a2wsgi.WSGIMiddleware]]:
+    return request.param  # type: ignore[return-value]
 
 
 @pytest.mark.anyio

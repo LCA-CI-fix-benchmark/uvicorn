ec7ff78521cd04793160a665ed506bc6d58342f1
ubuntu <ubuntu@ip-10-0-1-44>
2025-01-18 10:52:37 +0000

    waleko__claude-3.5-sonnet_ce59__id_147

--- tests/middleware/test_wsgi.py
+++ tests/middleware/test_wsgi.py
@@ -1,6 +1,6 @@
 import io
 import sys
-from typing import AsyncGenerator, Callable, List
+from typing import AsyncGenerator, Callable, List, Type, Union
 
 import a2wsgi
 import httpx
@@ -51,7 +51,7 @@ def return_exc_info(environ: Environ, start_response: StartResponse) -> List[byt
 
 
 @pytest.fixture(params=[wsgi._WSGIMiddleware, a2wsgi.WSGIMiddleware])
-def wsgi_middleware(request: pytest.FixtureRequest) -> Callable:
+def wsgi_middleware(request: pytest.FixtureRequest) -> Type[Union[wsgi._WSGIMiddleware, a2wsgi.WSGIMiddleware]]:
     return request.param
 
 
